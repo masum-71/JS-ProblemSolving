73e89f66a0f0e04b76b3efdfde824dd6d6f6481d
Masum Billah <masum.monpura@gmail.com>
2022-08-08 16:31:40 +0600

    1st solved

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //Problem:1
+
+let momGive = 1000;
+let orangePrice = 300;
+let applePrice = 400;
+
+let totalCost = orangePrice + applePrice;
+let shopKeeperReturn = momGive - totalCost;
+console.log(shopKeeperReturn);
+//*************************************** */
