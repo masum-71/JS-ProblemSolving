238750c41c1cd4382a0acf38a6d35751ac8c6b7b
Masum Billah <masum.monpura@gmail.com>
2022-08-08 19:22:23 +0600

    2nd solved

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ let totalCost = orangePrice + applePrice;
 let shopKeeperReturn = momGive - totalCost;
 console.log(shopKeeperReturn);
 //*************************************** */
+
+// Problem:2
+
+let math = 78;
+let bio = 54;
+let chemistry = 65;
+let physics = 70;
+let bangla = 85;
+
+let totalMarks = math + bio + chemistry + physics + bangla;
+let avgMarks = totalMarks / 5;
+console.log(avgMarks);
+//*************************************** */
