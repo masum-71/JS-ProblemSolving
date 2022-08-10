2af42f1eab1328e5d7c8d757e98d045fdd2e928b
Masum Billah <masum.monpura@gmail.com>
2022-08-10 11:17:23 +0600

    merged

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ let totalCost = orangePrice + applePrice;
 let shopKeeperReturn = momGive - totalCost;
 console.log(shopKeeperReturn);
 //*************************************** */
+
+//Problem: 2
+let math = 75;
+let bio = 65;
+let chemistry = 54;
+let physics = 78;
+let bangla = 90;
+
+let totalMarks = math + bio + chemistry + physics + bangla;
+let avgMarks = totalMarks/5;
+console.log(avgMarks)
